59d9dfb42c340353a8ef28015c285f4b23a083f2
diogomcasado <61639098+diogomcasado@users.noreply.github.com>
2022-02-10 19:05:43 +0000

    working with .xlsx/.xls

--- XLSXtoCSV.py
+++ XLSXtoCSV.py
@@ -1,11 +1,7 @@
-#pip install pandas
-#pip install tqdm
-#pip install xlrd==1.2.0
-#pip install openpyxl
-
 from tqdm import tqdm
 import pandas as pd
-import os, fnmatch
+import os
+import fnmatch
 import shutil
 from os import path, system
 
@@ -15,21 +11,22 @@ folder = os.path.dirname(os.path.abspath(__file__))
 
 print('Pasta: ', folder)
 
-#funcao para verificar/criar pasta e mover .xlsx
+
+# funcao para verificar/criar pasta e mover .xlsx
 def checkCreateFolder():
     newFolder = ('xlsx')
     pastaCriar = os.path.join(folder, newFolder)
 
     if not os.path.isdir(pastaCriar):
         os.mkdir(pastaCriar)
-    
-    for x in tqdm(resultadosXLSX):
-        if x.endswith ('.xlsx'):
-            path = findFile(x, folder)
-            #os.path.dirname(path)
-            shutil.move(path,pastaCriar)
 
-#funcao para ter lista de resultados
+    for x in resutados_excel:
+        if x.endswith('.xls') or x.endswith('.xlsx'):
+            path = findFile(x, folder)
+            shutil.move(path, pastaCriar)
+
+
+# funcao para ter lista de resultados
 def find(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
@@ -38,50 +35,59 @@ def find(pattern, path):
                 result.append(name)
     return result
 
-#funcao para ter caminho do ficheiro
+
+# funcao para ter caminho do ficheiro
 def findFile(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
 
-resultadosXLSX = (find('*.xlsx', folder))
+resultados_XLSX = (find('*.xlsx', folder))
+resultados_XLS = (find('*.xls', folder))
 
-#verifica se há ficheiros .xlsx
-if (len(resultadosXLSX) == 0):
-    print('Erro: Ficheiros .XLSX não encontrados')
+resutados_excel = resultados_XLS + resultados_XLSX
+
+
+# verifica se há ficheiros .xlsx
+if (len(resutados_excel) == 0):
+    print('Erro: Ficheiros .XLSX/.XLS não encontrados')
     exit()
 else:
-    print('Ficheiros .XLSX encontrados:' ,len(resultadosXLSX))
+    if not len(resultados_XLSX) == 0:
+        print('Ficheiros .XLSX encontrados:', len(resultados_XLSX))
+    if not len(resultados_XLS) == 0:
+        print('Ficheiros .XLS encontrados:', len(resultados_XLS))
 
 
-#xlsx to csv
-print("\nA tranformar ficheiros .XLSX em .CSV...")
-for x in tqdm(resultadosXLSX):
-    if x.endswith ('.xlsx'):
-        xlsxToCsvOk = True
-        #print(x)
-        nomeFinal = x.replace(".xlsx", ".csv")
+# transforma xlsx e xls para csv
+print("\nA tranformar ficheiros .XLSX/.XLS em .CSV...")
+for x in tqdm(resutados_excel):
 
-        path = findFile(x, folder)
+    if x.endswith('.xlsx'):
+        nome_final = x.replace(".xlsx", ".csv")
+    elif x.endswith('.xls'):
+        nome_final = x.replace(".xls", ".csv")
+    else:
+        continue
 
-        data_xls = pd.read_excel(path, dtype=str, index_col=None)
-        data_xls.to_csv(os.path.join(os.path.dirname(path), nomeFinal), encoding='utf-8', index=False, sep=';')   
-        
+    xlsxToCsvOk = True
+    path = findFile(x, folder)
 
-print("\nFicheiros .XLXS transformados")
+    data_xls = pd.read_excel(path, dtype=str, index_col=None)
+    data_xls.to_csv(os.path.join(os.path.dirname(path), nome_final),
+                    encoding='utf-8', index=False, sep=';')
+
+print("\nFicheiros transformados")
+
+
+# pergunta ao utilizador
 resposta = ''
 while resposta == '':
-    resposta = input ("Mover .XLSX para uma pasta?(1-sim 2-nao)")
+    resposta = input("Mover .XLSX/.XLS para uma pasta?(1-sim 2-nao)")
     if resposta == '1':
-        print("a mover")
+        print("A mover")
         checkCreateFolder()
     elif resposta == '2':
         exit()
     elif resposta != '1':
         resposta = ''
-
-
-
-
-
-
